eb33cd2d6fdddb53ecc96040a9b86995992f8451
Charles Edwards <cedwards@smithdrug.com>
2017-08-02 11:59:10 -0400

    commit

--- app/app.js
+++ app/app.js
@@ -1,1 +1,57 @@
 //define you angular app here and routing too
+var hD = angular.module('helpDesk', ['ui.router',
+'ngStorage',
+'ui.bootstrap',
+'uiRouterStyles',
+'wt.responsive',
+'infinite-scroll'
+]);
+
+    hD.values('globals', {
+        url: 'http://staging2.smithdrug.com'
+    })
+    .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/');
+        $stateProvider
+            .state('orderStats', {
+                url:'/',
+                params: {
+                    obj: null
+                },
+                templateUrl: 'app/views/orderStats.html',
+                controller: 'orderStatsCtrl'
+            })
+            .state('itSupport', {
+                url:'/',
+                params: {
+                    obj: null
+                },
+                templateUrl: 'app/views/itSupport.html',
+                controller: 'itSupportCtrl'
+            })
+            .state('customerAdmin', {
+                url:'/',
+                params: {
+                    obj: null
+                },
+                templateUrl: 'app/views/customerAdmin.html',
+                controller: 'custAdminCtrl'
+            })
+            .state('onr', {
+                url:'/',
+                params: {
+                    obj: null
+                },
+                templateUrl: 'app/views/onr.html',
+                controller: 'onrCtrl'
+            })
+            .state('express222Setup', {
+                url:'/',
+                params: {
+                    obj: null
+                },
+                templateUrl: 'app/views/express222Setup.html',
+                controller: 'express222Ctrl'
+            });
+
+    }]);
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,1 +1,45 @@
 // first controller here
+angular.module('helpDesk').controller('MainCtrl', function ($state, $rootScope, $scope, $localStorage, $sessionStorage, $q) {
+	// CONSTANTS //
+	//////////////
+	$scope.$storage = $sessionStorage;
+	$scope.$localStorage = $localStorage;
+	$scope.navbar = [
+		{
+			name: 'Order Stats',
+			route: 'orderStats'
+		},
+		{
+			name: 'Customer Info',
+			route: 'itSupport'
+		},
+		{
+			name: 'Customer Admin',
+			route: 'customerAdmin'
+		},
+		{
+			name: 'Orders Not Received',
+			route: 'onr'
+		},
+        {
+            name: 'Express 222 Setup',
+            route: 'express222Setup'
+        }
+	];
+
+    // FUNCTIONS //
+    //////////////
+	function initMainCtrl() {
+
+	}
+
+    /// SCOPE FUNCTIONS ///
+    //////////////////////
+
+
+
+    /// INIT ///
+    ///////////
+    initMainCtrl();
+
+});
