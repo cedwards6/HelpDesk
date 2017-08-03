9da296588b46c07e52743a4d1016adcb540c3b11
Charles Edwards <cedwards@smithdrug.com>
2017-08-03 12:22:18 -0400

    commit

---
+++ app/scripts/controllers/itSupport.js
@@ -0,0 +1,17 @@
+angular.module('helpDesk').controller('itSupportCtrl', function ($state, $rootScope, $scope, $uibModal, $localStorage, $sessionStorage, $q) {
+
+
+
+    // FUNCTIONS //
+    //////////////
+    function inititSupportCtrl() {
+        console.log('it support');
+
+    }
+
+
+    // INIT //
+    /////////
+    inititSupportCtrl();
+
+});
