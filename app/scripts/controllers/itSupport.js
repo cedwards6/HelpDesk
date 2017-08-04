b2f8cefb60d3e9095422c8487c2fccca1de492c2
Charles Edwards <cedwards@smithdrug.com>
2017-08-04 16:48:00 -0400

    added itSupport page outline

--- app/scripts/controllers/itSupport.js
+++ app/scripts/controllers/itSupport.js
@@ -1,11 +1,21 @@
 angular.module('helpDesk').controller('itSupportCtrl', function ($state, $rootScope, $scope, $uibModal, $localStorage, $sessionStorage, $q) {
 
-
+    var customerId;
 
     // FUNCTIONS //
     //////////////
     function inititSupportCtrl() {
-        console.log('it support');
+        //console.log('it support');
+
+    }
+
+    /// SCOPE FUNCTIONS ///
+    //////////////////////
+
+    $scope.search = function() {
+        console.log('in search', $scope.searchAcct);
+        customerId = $scope.searchAcct;
+        // make webservice calls here
 
     }
 
